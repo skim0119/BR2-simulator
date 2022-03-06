9030465f7dc52b0909c7ccae5f6267d70f539116
Seung Hyun Kim <skim449@illinois.edu>
2022-03-06 17:17:13 -0600

    add customized elastica modulesg

---
+++ BR2/custom_activation.py
@@ -0,0 +1,83 @@
+import numpy as np
+from elastica._linalg import _batch_matvec
+from elastica.utils import _bspline
+
+import numba
+from numba import njit
+from elastica._elastica_numba._linalg import _batch_product_i_k_to_ik
+from elastica._elastica_numba._external_forces import NoForces
+
+
+class PointForces(NoForces):
+    """
+    This class applies constant forces on the endpoint nodes.
+
+        Attributes
+        ----------
+        start_force: numpy.ndarray
+            2D (dim, 1) array containing data with 'float' type. Force applied to first node of the rod-like object.
+        end_force: numpy.ndarray
+            2D (dim, 1) array containing data with 'float' type. Force applied to last node of the rod-like object.
+        ramp_up_time: float
+            Applied forces are ramped up until ramp up time.
+
+    """
+
+    def __init__(self, force, location, ramp_up_time=1e-3):
+        """
+
+        Parameters
+        ----------
+        start_force: numpy.ndarray
+            2D (dim, 1) array containing data with 'float' type.
+            Force applied to first node of the rod-like object.
+        end_force: numpy.ndarray
+            2D (dim, 1) array containing data with 'float' type.
+            Force applied to last node of the rod-like object.
+        ramp_up_time: float
+            Applied forces are ramped up until ramp up time.
+
+        """
+        super().__init__()
+        self.force = force
+        assert ramp_up_time >= 0.0
+        self.ramp_up_time = ramp_up_time
+        assert location < 1.0 and location > 0.0
+        self.location = location
+
+    def apply_forces(self, system, time=0.0):
+        force_index = int(system.external_forces.shape[-1]*self.location)
+        self.compute_end_point_forces(
+            system.external_forces,
+            self.force,
+            force_index,
+            time,
+            self.ramp_up_time,
+        )
+
+    @staticmethod
+    @njit(cache=True)
+    def compute_end_point_forces(
+        external_forces, force, force_index, time, ramp_up_time
+    ):
+        """
+        Compute end point forces that are applied on the rod using numba njit decorator.
+
+        Parameters
+        ----------
+        external_forces: numpy.ndarray
+            2D (dim, blocksize) array containing data with 'float' type. External force vector.
+        force: numpy.ndarray
+            2D (dim, 1) array containing data with 'float' type.
+        force_index: int
+            index of the rod
+        time: float
+        ramp_up_time: float
+            Applied forces are ramped up until ramp up time.
+
+        Returns
+        -------
+
+        """
+        factor = min(1.0, time / ramp_up_time)
+        external_forces[..., force_index] += force * factor
---
+++ BR2/custom_constraint.py
@@ -0,0 +1,87 @@
+import numpy as np
+from elastica._elastica_numba._rotations import _get_rotation_matrix
+from elastica._elastica_numba._boundary_conditions import FreeRod
+
+import numba
+from numba import njit
+
+class LastEndFixedRod(FreeRod):
+    """
+    This boundary condition class fixes one end of the rod. Currently,
+    this boundary condition fixes position and directors
+    at the first node and first element of the rod.
+        Attributes
+        ----------
+        fixed_positions : numpy.ndarray
+            2D (dim, 1) array containing data with 'float' type.
+        fixed_directors : numpy.ndarray
+            3D (dim, dim, 1) array containing data with 'float' type.
+    """
+
+    def __init__(self, fixed_position, fixed_directors):
+        """
+        Parameters
+        ----------
+        fixed_position : numpy.ndarray
+            2D (dim, 1) array containing data with 'float' type.
+        fixed_directors : numpy.ndarray
+            3D (dim, dim, 1) array containing data with 'float' type.
+        """
+        FreeRod.__init__(self)
+        self.fixed_position = fixed_position
+        self.fixed_directors = fixed_directors
+
+    def constrain_values(self, rod, time):
+        # rod.position_collection[..., 0] = self.fixed_position
+        # rod.director_collection[..., 0] = self.fixed_directors
+        self.compute_contrain_values(
+            rod.position_collection,
+            self.fixed_position,
+            rod.director_collection,
+            self.fixed_directors,
+        )
+
+    def constrain_rates(self, rod, time):
+        # rod.velocity_collection[..., 0] = 0.0
+        # rod.omega_collection[..., 0] = 0.0
+        self.compute_constrain_rates(rod.velocity_collection, rod.omega_collection)
+
+    @staticmethod
+    @njit(cache=True)
+    def compute_contrain_values(
+        position_collection, fixed_position, director_collection, fixed_directors
+    ):
+        """
+        Computes constrain values in numba njit decorator
+        Parameters
+        ----------
+        position_collection : numpy.ndarray
+            2D (dim, blocksize) array containing data with `float` type.
+        fixed_position : numpy.ndarray
+            2D (dim, 1) array containing data with 'float' type.
+        director_collection : numpy.ndarray
+            3D (dim, dim, blocksize) array containing data with `float` type.
+        fixed_directors : numpy.ndarray
+            3D (dim, dim, 1) array containing data with 'float' type.
+        Returns
+        -------
+        """
+        position_collection[..., -1] = fixed_position
+        director_collection[..., -1] = fixed_directors
+
+    @staticmethod
+    @njit(cache=True)
+    def compute_constrain_rates(velocity_collection, omega_collection):
+        """
+        Compute contrain rates in numba njit decorator
+        Parameters
+        ----------
+        velocity_collection : numpy.ndarray
+            2D (dim, blocksize) array containing data with `float` type.
+        omega_collection : numpy.ndarray
+            2D (dim, blocksize) array containing data with `float` type.
+        Returns
+        -------
+        """
+        velocity_collection[..., -1] = 0.0
+        omega_collection[..., -1] = 0.0
