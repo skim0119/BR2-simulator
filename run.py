88fb5d70f34fd255cc3f20755505dde80ca0d87c
Seung Hyun Kim <skim449@illinois.edu>
2022-03-16 19:44:12 -0500

    update

--- run.py
+++ run.py
@@ -1,45 +1,41 @@
 import os
 import sys
 
+# sys.settrace
+
 import numpy as np
+
 np.set_printoptions(precision=4)
 
-#sys.settrace
 from br2.environment import Environment
 
 import argparse
 
+
 def main():
     # Argument Parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('--tag', type=str, required=True)
+    parser.add_argument("--tag", type=str, required=True)
     args = parser.parse_args()
 
     # Action Configuration
     psi2Nm2 = 6895
 
     # Actuation Profile
-    action = {
-        "action1": 40 * psi2Nm2,
-        "action2": 0 * psi2Nm2
-    }
+    action = {"action1": 35 * psi2Nm2, "action2": 0 * psi2Nm2}
 
     # Prepare environment
     env = Environment(
         run_tag=args.tag,
         final_time=simulation_time,
-        flag_collect-data=True,
     )
     env.reset(
-        rod_database_path='sample_database/sample_rod_library.json',
-        assembly_config_path='sample_assembly/single_br2_v1.json'
+        rod_database_path="sample_database/sample_rod_library.json",
+        assembly_config_path="sample_assembly/single_br2_v1.json",
     )
 
     # Simulation
-    env.run(
-        action=action,
-        duration=20.0
-    )
+    env.run(action=action, duration=20.0)
 
     # Post Processing
     env.post_processing(
@@ -59,6 +55,7 @@ def main():
 
     # Terminate
     env.close()
+
 
 if __name__ == "__main__":
     main()
