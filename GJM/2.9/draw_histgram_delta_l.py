1fbc369d519a2de02782c4708ece912b0cdc4443
StevenGuo30 <1647545809@qq.com>
2024-02-09 15:50:51 -0600

    2.9 add draw 3d diagram of delta_L

--- GJM/2.9/draw_histgram_delta_l.py
+++ GJM/2.9/draw_histgram_delta_l.py
@@ -1,14 +1,48 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('F:\\Soft_arm\\Code_br2\\BR2-simulator\\GJM\\2.9\\Disatance_Record.txt')
-data = data.reshape(-1,)
-print(np.shape(data))
+def Print_Histgram(data):
+    data = data.reshape(-1,)
+    
+    plt.figure()
+    
+    plt.hist(data, bins=30, alpha=0.5, edgecolor='black',density=True)
 
-plt.hist(data, bins=30, alpha=0.5, edgecolor='black',density=True)
+    plt.title('Histogram of delta L')  # 设置标题
+    plt.xlabel('delta L')  # 设置X轴标签
+    plt.ylabel('Frequency')  # 设置Y轴标签
 
-plt.title('Histogram of delta L')  # 设置标题
-plt.xlabel('delta L')  # 设置X轴标签
-plt.ylabel('Frequency')  # 设置Y轴标签
 
-plt.show()  # 显示图形
+    
+def Print_deltaL_3d(data):
+    n_elem = data.shape[1]
+    length = np.linspace(0,1,n_elem)
+    time_steps = np.arange(data.shape[0])
+    
+    # define data on X,Y plane
+    X,Y = np.meshgrid(length, time_steps)
+    Z = data
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    surf = ax.plot_surface(X,Y,Z,cmap='viridis')# viridis define the color
+    
+    ax.set_xlabel('L')
+    ax.set_ylabel('time')
+    ax.set_zlabel('delta_L')
+    
+
+    
+
+def main():
+    data = np.loadtxt('F:\\Soft_arm\\Code_br2\\BR2-simulator\\GJM\\2.9\\Disatance_Record.txt')
+    Print_Histgram(data)
+    Print_deltaL_3d(data)
+    plt.show()
+    
+    
+
+
+if __name__ == "__main__":
+    main()
