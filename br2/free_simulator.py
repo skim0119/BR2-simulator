8792fcf9c9b2e52bd766b10f1c57077f7c83061d
Seung Hyun Kim <skim449@illinois.edu>
2024-05-27 15:02:39 -0500

    move hollow specification loc

--- br2/free_simulator.py
+++ br2/free_simulator.py
@@ -176,17 +176,6 @@ class FreeAssembly:
                         break
 
                 rod = self.add_free(rod_name, actuation_name, **rod_spec)
-                if rod_spec["hollow"]:
-                    outer_radius = rod.outer_radius
-                    inner_radius = rod.inner_radius
-                    hollow_scale_bend = (
-                        (2 * outer_radius) ** 4 - (2 * inner_radius) ** 4
-                    ) / ((2 * outer_radius) ** 4)
-                    hollow_scale_shear = (
-                        (2 * outer_radius) ** 2 - (2 * inner_radius) ** 2
-                    ) / ((2 * outer_radius) ** 2)
-                    rod.bend_matrix = hollow_scale_bend * rod.bend_matrix
-                    rod.shear_matrix = hollow_scale_shear * rod.shear_matrix
 
                 self.free[rod_name] = rod
                 seg_rods.append(rod_name)
@@ -307,6 +296,18 @@ class FreeAssembly:
 
         # Append rod to simulator
         self.simulator.append(rod)
+
+        if getattr(rod_spec, "hollow", False):
+            outer_radius = rod.outer_radius
+            inner_radius = rod.inner_radius
+            hollow_scale_bend = (
+                (2 * outer_radius) ** 4 - (2 * inner_radius) ** 4
+            ) / ((2 * outer_radius) ** 4)
+            hollow_scale_shear = (
+                (2 * outer_radius) ** 2 - (2 * inner_radius) ** 2
+            ) / ((2 * outer_radius) ** 2)
+            rod.bend_matrix = hollow_scale_bend * rod.bend_matrix
+            rod.shear_matrix = hollow_scale_shear * rod.shear_matrix
 
         # add damping
         if "damping_constant" in rod_spec:
