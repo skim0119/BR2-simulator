9cbf34091d555a2e9ca2a7dbf5aa644d937e5ede
Seung Hyun Kim <skim0119@gmail.com>
2024-07-20 21:28:35 -0500

    fix bug

---
+++ validations/loader.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+
+def load_data(csv_path: str, x_key: str, y_key: str, keys: list[str] | None = None):
+    data = pd.read_csv(csv_path)
+
+    # Convert to numpy
+    activation = data[x_key]
+    result = data[y_key]
+    activation = np.array(activation)
+    result = np.array(result)
+
+    if keys is not None:
+        info = {}
+        breakpoint()
+        for key in keys:
+            info[key] = np.array(data[key])
+        return activation, result, info
+    return activation, result
